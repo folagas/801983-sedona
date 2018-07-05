afc2d03102938976e95386202f538e059646e90a
Ekaterina Chernetsova <papchnskaya@gmail.com>
2018-07-05 12:42:13 +0300

    Упорядочила код на главной, пока ничего не меняла

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var link = document.querySelector(".mybutton-order");
+var popup = document.querySelector(".hotel-choice-popup");
+
+link.addEventListener("click", function (evt) {
+evt.preventDefault();
+popup.classList.add("popup-show");
+});
