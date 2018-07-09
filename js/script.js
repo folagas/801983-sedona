4ba38f352a397443b8c0fcb20b1371011c60df68
Ekaterina Chernetsova <papchnskaya@gmail.com>
2018-07-09 19:45:42 +0300

    Охренеть, джаваскрипт починила как надо!

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+/*
 var link = document.querySelector(".mybutton-order");
 var popup = document.querySelector(".hotel-choice-popup");
 
+popup.classList.add("popup-hide");
 link.addEventListener("click", function (evt) {
 evt.preventDefault();
 popup.classList.toggle("popup-hide");
 });
+*/
